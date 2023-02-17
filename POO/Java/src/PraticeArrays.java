74f151befcf5dc692ea41df6ea321c03b4318d8b
unknown <hoyner26@gmail.com>
2023-02-17 00:08:33 -0600

    class praticeArrays

---
+++ POO/Java/src/PraticeArrays.java
@@ -0,0 +1,22 @@
+public class PraticeArrays {
+
+    public static void main(String[] args) {
+        /*int[] arreglo = new int[5];
+        //int arreglo arreglo2[] = new int[5]; es lo mismo que la linea de arriba
+        arreglo[0] = 5;
+        arreglo[1] = 38;
+        arreglo[2] = -15;
+        arreglo[3] = 92;
+        arreglo[4] = 71;*/
+
+        int[] arreglo = {5, 38, -15, 92, 71};
+        
+        
+        
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println(arreglo[i]);
+        }
+        
+    }
+    
+}
