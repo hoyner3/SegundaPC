aafee6067dc552e14f6f8284cffd9c39a088b00d
unknown <hoyner26@gmail.com>
2023-02-18 12:44:08 -0600

    nuevo proyecto aprendiendo poo

---
+++ POO/practica de class/src/poo/Uso_Coche.java
@@ -0,0 +1,11 @@
+package poo;
+
+public class Uso_Coche {
+    public static void main(String[] args) {
+
+        Coche Renault = new Coche(); // Instanciar un objeto de la clase Coche (crear un objeto).Ejemplar de la clase Coche.
+        System.out.println("El coche tiene " + Renault.ruedas + " ruedas");
+        
+    }
+    
+}
