f504ca3ac330e53e7bf1b06f4562c63cf3e56382
unknown <hoyner26@gmail.com>
2023-02-18 18:32:37 -0600

    nuevo proyecto aprendiendo poo progreso

--- POO/practica de class/src/poo/Coche.java
+++ POO/practica de class/src/poo/Coche.java
@@ -1,10 +1,16 @@
 package poo;
 public class Coche {
-    int ruedas;
-    int largo;
-    int ancho;
-    int motor;
-    int peso;
+    // Atributos o propiedades (variables)
+    //el private es encapsulamiento
+    private int ruedas;
+    private int largo;
+    private int ancho;
+    private int motor;
+    private int peso_plataforma;
+    
+    String color;
+    int peso_total;
+    boolean asientos_cuero, climatizador;
 
     // Constructor
     public Coche(){
@@ -12,6 +18,28 @@ public class Coche {
         largo = 2000;
         ancho = 300;
         motor = 1600;
-        peso = 500;
+        peso_plataforma = 500;
+
     }
+
+    // Metodo Getter - dime_Largo - devuelve el valor de la variable largo
+    public String dime_largo(){//GETTER
+
+        return"El largo del coche es " + largo;
+    
+    }
+
+    // Metodo Setter - establece_color - El void es que no devuelve nada
+    public void establece_color(){ //SETTER
+
+        color = "Azul";
+
+    }
+
+    public String dime_color(){//GETTER
+
+        return "El color del coche es " + color;
+    
+    }
+
 }
--- POO/practica de class/src/poo/Uso_Coche.java
+++ POO/practica de class/src/poo/Uso_Coche.java
@@ -2,10 +2,12 @@ package poo;
 
 public class Uso_Coche {
     public static void main(String[] args) {
+        // este es el objeto
+        Coche micoche = new Coche();
 
-        Coche Renault = new Coche(); // Instanciar un objeto de la clase Coche (crear un objeto).Ejemplar de la clase Coche.
-        System.out.println("El coche tiene " + Renault.ruedas + " ruedas");
-        
+        micoche.establece_color();
+
+        System.out.println(micoche.dime_color());
     }
     
 }
