24b869edb49e2f56dd49f160b3dedcc4af21c55c
unknown <hoyner26@gmail.com>
2023-02-21 14:00:27 -0600

    ejecucion de intruciones

--- Python/ejecucciones de intrucciones.py
+++ Python/ejecucciones de intrucciones.py
@@ -1,6 +1,7 @@
 #Programa para ejecutar intrucciones.
 
 def binario_a_decimal(binario):
+    #print(type(binario))
     decimal = 0
     for i in range(len(binario)):
         decimal += int(binario[i]) * 2 ** (len(binario) - i - 1)
@@ -14,6 +15,7 @@ def decimal_a_binario(decimal):
     return binario
 def setInstruccion(instruccion,memoria,dir1,dir2,ac):
     dir = 0
+    temp = 0
     if instruccion == 1:
         # 1- Carga de memoria 1 hacia AC
         dir1 = binario_a_decimal(dir1)#se pasa de binario a decimal
@@ -24,8 +26,10 @@ def setInstruccion(instruccion,memoria,dir1,dir2,ac):
     elif instruccion == 2:
         #2- Almacenar en memoria 1 desde AC
         dir1 = binario_a_decimal(dir1)
-        dir = memoria.get(str(dir1))
+        #dir = memoria.get(str(dir1))
+        ac = decimal_a_binario(ac)
         memoria[str(dir1)] = ac #se guarda en la memoria
+        ac = binario_a_decimal(ac)
         return ac
     elif instruccion == 3:
         # 3- Suma: memoria 1 + AC
@@ -54,17 +58,17 @@ def setInstruccion(instruccion,memoria,dir1,dir2,ac):
     elif instruccion == 6:
         #6- Resta: AC – memoria 1, almacena en memoria 2
         dir1 = binario_a_decimal(dir1)
-        dir = memoria.get(str(dir1))
-        dir = decimal_a_binario(dir)
+        dir = memoria.get(str(dir1))# se obtiene el valor de la memoria
+        #dir = decimal_a_binario(dir)
         dir = binario_a_decimal(dir)
-        ac = ac - dir#se resta el valor de la memoria con el ac
+        temp = ac - dir#se resta el valor de la memoria con el ac
         dir2 = binario_a_decimal(dir2)
-        memoria[str(dir2)] = ac#se guarda en la memoria
+        memoria[str(dir2)] = temp#se guarda en la memoria
         return ac
     elif instruccion == 7:
         # 7- Multiplicación: memoria 1 x AC, almacena en AC
         dir1 = binario_a_decimal(dir1)
-        dir = memoria.get(str(dir1))
+        dir = memoria.get(str(dir1))# se obtiene el valor de la memoria
         dir = binario_a_decimal(dir)
         ac = ac * dir#se multiplica el valor de la memoria con el ac
         return ac
@@ -83,22 +87,63 @@ def leer_archivo_a_diccionario():
         diccionario[clave] = valor
     archivo.close()
     return diccionario
-# El main es la funcion principal del programa
-def main():
-    print("Menu para ejecutar intrucciones")
-    
-    # diccionario de memoria
-    memoria = {"700":"00011001110010000000000000","701":"01001001110001010011100100","702":"00101001110010000000000000","703":"00011001110011100000000000","704":"01111001110001000000000000","705":"01101001110010010011100010","1250":"1100101","1251":"101000010010001000","1252":"1100101","1253":"11010111010","1254":"10000001110010000","1255":"11"}
-    # variables
-    pc = 0
-    ac = 0
-    intruccion = 0
-    dir1 = 0
-    dir2 = 0
-    numdato = 0
+
+#metodo que agrege direcciones a memoria
+def agregar_direcciones_a_memoria(memoria):
+    print("1. Agregar direcciones a memoria")
+    print("2. salir")
+    opcion = int(input("Ingrese una opcion: "))
+    if opcion == 1:
+        validacion = False
+        while validacion == False:
+            direccion = input("Ingrese la direccion con formato debe empezar con d- : ")
+            if direccion.startswith("d-"):
+                validacion = True
+                
+            else:
+                print("Direccion invalida")
+
+        validacion = False
+        while validacion == False:
+            print()
+            print()
+            print("Las unicas intrucciones que se pueden ingresar son: ")
+            print("1- Carga de memoria 1 hacia AC: 0001")
+            print("2- Almacenar en memoria 1 desde AC: 0010")
+            print("3- Suma: memoria 1 + AC: 0011")
+            print("4- Suma: memoria 1 + memoria 2 + AC: 0100")
+            print("5- Resta: AC – memoria 1, almacena en AC: 0101")
+            print("6- Resta: AC – memoria 1, almacena en memoria 2: 0110")
+            print("7- Multiplicación: memoria 1 x AC, almacena en AC: 0111")
+            dato = input("Ingrese el dato de la memoria con una cantidad de 26bits: ")
+            if  (len(dato) == 26 and dato.startswith("0")):
+                if (binario_a_decimal(dato[4:15]) >= 1250 and binario_a_decimal(dato[4:15]) <= 1255):
+                    validacion = True
+                else:
+                    print("Direccion invalida")
+                    print("Las direcciones de memoria deben estar entre 1250 y 1255")
+                
+            else:
+                print("Dato invalido")
+                print("El dato debe tener 26 bits y empezar con 0")
+                
+        
+        memoria[direccion] = dato 
+        print("Direccion agregada con exito")
+        print("Direccion: ",direccion,"Dato: ",dato)
+        
+        return memoria
+    else:
+        print("Saliendo...")
+        main()
+
+ 
+
+#metodo que ejecuta las intrucciones
+def ejecutar_intrucciones(memoria,pc,ac,intruccion,dir1,dir2,numdato):
     # recorrer el diccionario
     for clave,valor in memoria.items():
-        if clave[0:1] == "7":
+        if clave[0:1] == "d":
             print("---------------------------------")
             
             print(clave,valor)
@@ -123,7 +168,7 @@ def main():
             print("dir1:",dir1)
             print("dir2:",dir2)
             print("Dato:",numdato)
-            print("Memoria:",memoria.get(str(numdato)))
+            print("Memoria:",memoria.get(str(numdato)))# se obtiene el valor de la memoria
             #imprimir la instruccion
             if intruccion == 6:
                 print("Dato:",numdato2)
@@ -131,14 +176,50 @@ def main():
 
             
             print("---------------------------------")
+    
+    return memoria,pc,ac,intruccion,dir1,dir2,numdato
             #hacer metodos para guardar la informarcion en archivos.
-
-    crear_diccionario_a_archivo(memoria)
-    memoria = leer_archivo_a_diccionario()
-    print(memoria)
-
-
-       
-
-
+# El main es la funcion principal del programa
+def main():
+    print("Menu para ejecutar intrucciones")
+    
+    # diccionario de memoria
+    #global memoria 
+    memoria = {"d-700":"00011001110010000000000000","d-701":"01001001110001010011100100","d-702":"00101001110010000000000000","d-703":"00011001110011100000000000","d-704":"01111001110001000000000000","d-705":"01101001110010010011100010","1250":"1100101","1251":"101000010010001000","1252":"1100101","1253":"11010111010","1254":"10000001110010000","1255":"11"}
+    # variables
+    pc = 0
+    #global ac
+    ac = 0
+    #global intruccion
+    intruccion = 0 
+    #global dir1
+    dir1 = 0
+    #global dir2
+    dir2 = 0 
+    #global numdato
+    numdato = 0
+    bandera = True
+    while bandera == True:
+        print("1. Agregar direcciones a memoria")
+        print("2. Ejecutar intrucciones")
+        print("3. Imprimir memoria")
+        print("4. Salir")
+        opcion = int(input("Ingrese una opcion: "))
+        if opcion == 1:
+            agregar_direcciones_a_memoria(memoria)
+        elif opcion == 2:
+            #print(type(memoria["d-700"]))
+            #print(memoria)
+            ejecutar_intrucciones(memoria,pc,ac,intruccion,dir1,dir2,numdato)
+        elif opcion == 3:
+            crear_diccionario_a_archivo(memoria)
+            memoria = leer_archivo_a_diccionario()
+            print(memoria)
+        elif opcion == 4:
+            print("Saliendo...")
+            break
+        else:
+            print("Opcion invalida")
+            print("Ingrese una opcion valida")
+            
 main()
